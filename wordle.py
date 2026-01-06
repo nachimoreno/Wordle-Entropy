90c344db516354505a6069a7b61a66f67bb0b7ed
nachimoreno <nachi.moreno@gmail.com>
2026-01-06 13:34:03 +0100

    Information logic implementation

--- wordle.py
+++ wordle.py
@@ -1,9 +1,13 @@
 import csv
 import random
+import math
+import statistics
+import json
 
 WORDLE_CSV = "wordle.csv"
 
-def fetch_words(path: str) -> list:
+def fetch_words(path: str) \
+        -> list:
     words = []
 
     with open(path, 'r') as f:
@@ -12,18 +16,14 @@ def fetch_words(path: str) -> list:
         for row in reader:
             words.append((row[0], row[1], row[2]))
 
+    del words[0]
+
     return words
 
 
-def pick_goal_word(words: list) -> str:
-    while True:
-        choice = random.choice(words)
-        if choice[2] != "": break
+def isolate_words(words: list) \
+        -> list:
 
-    return choice[0]
-
-
-def isolate_words(words: list) -> list:
     wordlist = []
 
     for word in words:
@@ -32,56 +32,202 @@ def isolate_words(words: list) -> list:
     return wordlist
 
 
-def evaluate_input(word: str, wordlist: list) -> str:
-    passed_base_eval = True
-    passed_adv_eval = False
+def pick_goal_word(words: list) \
+        -> str:
+    while True:
+        choice = random.choice(words)
+        if choice[2] != "": break
 
-    if len(word) != 5: passed_base_eval = False
-    if len(word) > 5: print("Invalid input: Too many characters (must be 5).")
-    if len(word) < 5: print("Invalid input: Too few characters (must be 5).")
+    return choice[0]
 
-    if not word.isalpha():
-        passed_base_eval = False
-        print("Invalid input: Only alphabetic characters are allowed.")
 
-    if passed_base_eval:
-        if word in wordlist:
-            passed_adv_eval = True
-        else:
-            print("Invalid input: '{}' is not in the wordle set.".format(word))
+def take_input(wordlist: list,
+               guesses: list,) \
+        -> str:
 
-    if passed_base_eval & passed_adv_eval:
+    while True:
+
+        word = input("Guess a word: ").strip().lower()
+
+        if word in guesses:
+            print("Invalid input: Already entered.")
+            continue
+
+        if len(word) > 5:
+            print("Invalid input: Too many characters (must be 5).")
+            continue
+        if len(word) < 5:
+            print("Invalid input: Too few characters (must be 5).")
+            continue
+
+        if not word.isalpha():
+            print("Invalid input: Only alphabetic characters are allowed.")
+            continue
+
+        if word not in wordlist:
+            print(f"Invalid input: '{word}' is not in the wordle set.")
+            continue
+
+        guesses.append(word)
         return word
 
-    user_guess = input("\nGuess a valid word: ")
-    evaluate_input(user_guess, wordlist)
 
+def evaluate_guess(guess: str,
+                   goal_word: str) \
+        -> tuple[set[str], list[str]]:
 
-def evaluate_guess(guess: str, goal_word: str) -> None:
-    print(f"Evaluating {guess} against {goal_word}")
+    if guess is None or goal_word is None:
+        raise ValueError("Invalid input: guess cannot be None.")
 
     guess = guess.lower()
     goal_word = goal_word.lower()
 
-    guess_set = set(guess.split())
+    intersection = set(guess) & set(goal_word)
 
-    print(f"\nGuess: '{guess_set}'")
+    if len(intersection) == 0:
+        print("No matching letters found.")
+        return intersection, ['_','_','_','_','_']
+
+    matches = ['_','_','_','_','_']
+
+    for pos in range(5):
+        if guess[pos] == goal_word[pos]:
+            matches[pos] = guess[pos]
+
+    return intersection, matches
 
 
-def main():
+def update_game_state(known_letters: set[str],
+                      matches: list[str],
+                      guess:str,
+                      goal_word:str) \
+    -> None:
+
+    intersection, found_matches = evaluate_guess(guess, goal_word)
+
+    for pos in range(len(matches)):
+        if found_matches[pos] == '_': continue
+        matches[pos] = found_matches[pos]
+
+    known_letters.update(intersection)
+
+
+def print_info(known_letters: set[str],
+                matches: list[str]) \
+    -> None:
+
+    matchstring = ''.join(matches)
+
+    print(f"\nKnown positions: {matchstring}")
+    print(f"Known letters: {known_letters}\n")
+
+
+def game():
     words = fetch_words(WORDLE_CSV)
-    goal_word = pick_goal_word(words)
     words_list = isolate_words(words)
+    goal_word = pick_goal_word(words)
+
+    won = False
+    known_letters = set()
+    matches = ['_','_','_','_','_']
+    guesses = []
 
     for _ in range(6):
-        user_guess = input("Guess a word: ")
-        user_guess = evaluate_input(user_guess, words_list)
+        guess = take_input(words_list, guesses)
 
-        if user_guess == goal_word:
-            print(f"Correct! {goal_word} was the word to guess.")
+        if guess == goal_word:
+            print(f"\nCorrect! {goal_word.title()} was the word to guess. Well done!")
+            won = True
             break
 
-        evaluate_guess(user_guess, goal_word)
+        update_game_state(known_letters, matches, guess, goal_word)
+        print_info(known_letters, matches)
+
+    if not won:
+        print(f"\nYou ran out of guesses. The word was '{goal_word}'!")
 
 
-if __name__ == "__main__": main()
+def p_to_bits(p: float) -> float:
+    bits = -math.log2(p)
+    return bits
+
+
+def total_possible_matches(words,
+                           known_letters: set[str],
+                           matches: list[str]) \
+    -> int:
+
+    possible_matches = []
+
+    if matches == ['_','_','_','_','_']: char_match_available = False # To be set to false, but true for debugging
+    else: char_match_available = True
+
+    num_char_matches_possible = 0
+
+    if char_match_available:
+
+        for match in matches:
+
+            if not match == '_':
+                num_char_matches_possible += 1
+
+    for word in words:
+        if word == words[0]: continue
+
+        if known_letters.issubset(word[0]):
+
+            if char_match_available:
+                num_char_matches_found = 0
+
+                for pos in range(len(word[0])):
+
+                    if word[0][pos] == matches[pos]:
+                        num_char_matches_found += 1
+
+                if num_char_matches_found == num_char_matches_possible:
+                    possible_matches.append(word)
+                    continue
+
+            possible_matches.append(word)
+
+    return len(possible_matches)
+
+
+def compute_opener_scores():
+    opener_scores = {}
+    words = fetch_words(WORDLE_CSV)
+    answer_words = [word for word in words if word[2] != '']
+
+    ca = 0
+    for answer in answer_words:
+        ca += 1
+        cw = 0
+        for word in words:
+            cw += 1
+            print(f"{ca}/{len(answer_words)}: {answer[0]} |{cw}/{len(words)}: {word[0]}")
+
+            intersection, matches = evaluate_guess(word[0], answer[0])
+            tpm = total_possible_matches(words, intersection, matches)
+            p = tpm / len(words)
+            bits = p_to_bits(p)
+
+            if not word[0] in opener_scores:
+                opener_scores[word[0]] = [bits]
+            else:
+                opener_scores[word[0]].append(bits)
+
+    for word in opener_scores:
+        mean = statistics.mean(opener_scores[word])
+        opener_scores[word] = mean
+
+    sorted_opener_scores = sorted(opener_scores.items(), key=lambda dct: dct[1], reverse=True)
+
+    with open("opener_scores.json", "w", encoding="utf-8") as f:
+        json.dump(sorted_opener_scores, f, indent=4)
+
+
+def analysis():
+    compute_opener_scores()
+
+
+if __name__ == "__main__": analysis()
